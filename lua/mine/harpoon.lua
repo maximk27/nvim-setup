ffc04f7c1c60cadc2a09f845e86554df64733ffa
maxim kim <maximk@umich.edu>
2025-12-24 20:48:07 -0500

    feat: show notification when mark not set

--- lua/mine/harpoon.lua
+++ lua/mine/harpoon.lua
@@ -3,6 +3,9 @@ local harpoon = require("harpoon")
 -- REQUIRED
 harpoon:setup()
 -- REQUIRED
+vim.keymap.set("n", "<C-e>", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
 
 local function set_mark(idx)
 	local path = vim.fn.expand("%:p")
@@ -20,16 +23,26 @@ local function set_mark(idx)
 	harpoon:list():replace_at(idx, { value = path })
 end
 
+local function go_mark(idx)
+	local item = harpoon:list():get(idx)
+	if item == nil then
+		vim.notify("Harpoon: mark not set", vim.log.levels.WARN)
+		return
+	end
+
+	harpoon:list():select(idx)
+end
+
 -- asdf source files
+-- qwe test
 -- r readme
--- t test
 
 -- setup mappings for A,S,D,F mapped to 1,2,3,4
-for idx, letter in ipairs({ "A", "S", "D", "F", "R", "T" }) do
+for idx, letter in ipairs({ "a", "s", "d", "f", "q", "w", "e", "r" }) do
 	vim.keymap.set("n", "m" .. letter, function()
 		set_mark(idx)
 	end)
 	vim.keymap.set("n", "'" .. letter, function()
-		harpoon:list():select(idx)
+		go_mark(idx)
 	end)
 end
